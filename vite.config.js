bcf040d2846a165b8c3fadf0bb4b50e3e7e1e371
ktzanak <tzanakiskon@gmail.com>
2025-02-14 07:11:12 +0100

    configuring host and port for vite

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  //port for the nginx need to be configured
+  server: {
+    port: 8080,
+    host: "localhost",
+  },
 });
