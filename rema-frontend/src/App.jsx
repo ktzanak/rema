4ffb7f72e4e9e934498793b38d855469195e7d90
ktzanak <tzanakiskon@gmail.com>
2025-03-04 15:48:58 +0100

    uploading the images and App main jsx

---
+++ rema-frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import Header from "./components/Header";
+import IngredientsInstructions from "./components/IngredientsInstructions";
+import "./css/app.css";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <br />
+      <br />
+      <IngredientsInstructions />
+    </div>
+  );
+}
+
+export default App;
