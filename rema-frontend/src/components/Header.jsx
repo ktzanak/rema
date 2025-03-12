03a89f928d64bb91418a2c1c028fe9431c3caa44
ktzanak <tzanakiskon@gmail.com>
2025-03-12 06:54:29 +0100

    adding links for the new routes

--- rema-frontend/src/components/Header.jsx
+++ rema-frontend/src/components/Header.jsx
@@ -1,10 +1,12 @@
 import styles from "../css/header.module.css";
+import { Link } from "react-router-dom";
+import bwLogo from "../assets/bw_remalogo.png";
 
 export default function Header() {
   return (
     <div className={styles.headerdeco}>
       <div className={styles.parentdiv}>
-        <img className={styles.imgpos} src="./images/bw_remalogo.png" />
+        <img className={styles.imgpos} src={bwLogo} />
         <div>
           <div className={styles.headersize}>Welcome to ReMa!</div>
           <hr className={styles.line} />
@@ -13,6 +15,14 @@ export default function Header() {
             type of recipe, main courses, cocktails, desserts and many more!
           </div>
         </div>
+      </div>
+      <div className={styles.navLinks}>
+        <Link to="/add" className={styles.navLink}>
+          Add Recipe
+        </Link>
+        <Link to="/list" className={styles.navLink}>
+          List Recipes
+        </Link>
       </div>
     </div>
   );
