9f32def26f530b5ceefec1d40a8d220735b566c8
ktzanak <tzanakiskon@gmail.com>
2025-03-18 07:15:57 +0100

    adding connection retries to avoid time-related connection errors

--- rema-backend/server.js
+++ rema-backend/server.js
@@ -8,26 +8,46 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// MySQL Connection
-const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
-});
+// Retry count
+let retryCount = 0;
+const maxRetries = 3;
 
-db.connect((err) => {
-  if (err) {
-    console.error("Database connection failed:", err);
-    return;
-  }
-  console.log("Connected to MySQL database");
-});
+function handleDatabaseConnection() {
+  const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+  });
+
+  db.connect((err) => {
+    if (err) {
+      retryCount += 1;
+      console.log(
+        `Error connecting to MySQL, retrying in 5 seconds... (${retryCount}/${maxRetries})`
+      );
+      if (retryCount < maxRetries) {
+        setTimeout(handleDatabaseConnection, 5000); // Retry after 5 seconds
+      } else {
+        console.log(
+          "Max retries reached. Could not connect to MySQL database."
+        );
+      }
+      return;
+    }
+
+    console.log("Connected to MySQL database");
+  });
+
+  return db;
+}
+
+const db = handleDatabaseConnection();
 
 // Routes
 app.get("/", (req, res) => {
-  res.send("Backend is running!");
+  res.send("Frontend, backend and database are running!");
 });
 
 // Fetch data
@@ -43,8 +63,7 @@ app.get("/listrecipes", (req, res) => {
 
 // Submit data
 app.post("/addrecipes", (req, res) => {
-  const { name } = req.body;
-  if (!name) return res.status(400).json({ error: "Name is required" });
+  const { title, description, cooking_time, portions, created_at } = req.body;
 
   db.query(
     "INSERT INTO recipes (id,title,description,cooking_time,portions,created_at) VALUES (?,?,?,?,?,?)",
@@ -53,7 +72,7 @@ app.post("/addrecipes", (req, res) => {
       if (err) {
         res.status(500).json({ error: err.message });
       } else {
-        res.json({ id: results.insertId, name });
+        res.json({ id: results.insertId, title });
       }
     }
   );
