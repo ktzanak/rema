35a906b3f0946c84dd6012d98b6a04ec05523d31
ktzanak <tzanakiskon@gmail.com>
2025-04-14 08:50:13 +0200

    included buffer in vite configuration

--- rema-frontend/vite.config.js
+++ rema-frontend/vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -10,5 +9,8 @@ export default defineConfig({
     watch: {
       usePolling: true,
     },
+  },
+  optimizeDeps: {
+    include: ["buffer"],
   },
 });
