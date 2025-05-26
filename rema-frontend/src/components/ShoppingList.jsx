b657b903b3fe1b561b52d3fd61e383dd6d2af558
ktzanak <tzanakiskon@gmail.com>
2025-05-26 19:19:18 +0200

    initializing the shopping list component

---
+++ rema-frontend/src/components/ShoppingList.jsx
@@ -0,0 +1,6 @@
+import React, { useState, useEffect } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+
+export default function ShoppingList() {
+  return <div>shopping list</div>;
+}
