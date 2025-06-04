21caa9d2feffb0170d53352f9c31d278cbf63b1a
ktzanak <tzanakiskon@gmail.com>
2025-06-04 09:13:57 +0200

    renamed components

--- rema-frontend/src/components/ShoppingList.jsx
+++
@@ -1,6 +0,0 @@
-import React, { useState, useEffect } from "react";
-import { Container, Row, Col } from "react-bootstrap";
-
-export default function ShoppingList() {
-  return <div>shopping list</div>;
-}
