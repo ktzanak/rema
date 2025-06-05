8deda6d67005d16bbb2cd24cc19926cf989e96a3
ktzanak <tzanakiskon@gmail.com>
2025-06-05 07:37:25 +0200

    changing names and references of components

---
+++ rema-frontend/src/components/ShoppingListPdf.jsx
@@ -0,0 +1,6 @@
+import React, { useState, useEffect } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+
+export default function ShoppingListPdf() {
+  return <div>shopping list</div>;
+}
