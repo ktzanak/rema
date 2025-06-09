f619434261783799e9cb595f1866c007c4099712
ktzanak <tzanakiskon@gmail.com>
2025-06-09 18:05:42 +0200

    adjusted comments and info of the Header

--- rema-frontend/src/components/Header.jsx
+++ rema-frontend/src/components/Header.jsx
@@ -5,19 +5,18 @@ import bwLogo from "../assets/bw_remalogo.png";
 export default function Header() {
   return (
     <header className={styles.header}>
-      {/*<div className={styles.headerdeco}>
-          <div className={styles.headersize}>Welcome to ReMa!</div>
-          <hr className={styles.line} />
-          <div className={styles.subheadersize}>
-            A simple application for creating recipes. It can be used for any
-            type of recipe, main courses, cocktails, desserts and many more!
-          </div>
-        </div>*/}
+      {/*
+      A simple application for creating recipes. It can be used for any
+      type of recipe, main courses, cocktails, desserts and many more!
+      */}
       <div className={styles.leftSection}>
         <img className={styles.logo} src={bwLogo} alt="ReMa logo" />
         <span className={styles.title}>ReMa</span>
       </div>
       <nav className={styles.navLinks}>
+        {/*<Link to="/home" className={styles.navLink}>
+          Home
+        </Link>*/}
         <Link to="/add" className={styles.navLink}>
           New recipe
         </Link>
