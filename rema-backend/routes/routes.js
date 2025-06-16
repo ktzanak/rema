b7bf7c4f6cfcf800294a70a93cfc6d8e3a5891a1
ktzanak <tzanakiskon@gmail.com>
2025-06-16 08:52:17 +0200

    added key and route for food quotes

--- rema-backend/routes/routes.js
+++ rema-backend/routes/routes.js
@@ -6,6 +6,7 @@ import {
   updaterecipe,
   raterecipe,
   askai,
+  foodQuote,
 } from "../controllers/recipeController.js";
 
 const router = express.Router();
@@ -16,6 +17,14 @@ router.delete("/deleterecipe/:recipeid", deleterecipe);
 router.put("/updaterecipe/:recipeid", updaterecipe);
 router.post("/raterecipe/:recipeid", raterecipe);
 router.post("/askai/:recipeid", askai);
+router.get("/hasFoodQuotesKey", (req, res) => {
+  if (process.env.QUOTES_API_KEY && process.env.QUOTES_API_KEY.trim() !== "") {
+    res.json({ ok: true });
+  } else {
+    res.json({ ok: false });
+  }
+});
+router.get("/foodQuote", foodQuote);
 router.get("/hasOpenaiKey", (req, res) => {
   if (process.env.OPENAI_API_KEY && process.env.OPENAI_API_KEY.trim() !== "") {
     res.json({ ok: true });
