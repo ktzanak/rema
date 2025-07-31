3a724310dc5debada594ed527106987bbc87385c
ktzanak <tzanakiskon@gmail.com>
2025-07-31 07:33:24 +0200

    tracking of the current pathname

--- rema-frontend/src/components/Header.jsx
+++ rema-frontend/src/components/Header.jsx
@@ -1,9 +1,13 @@
 import styles from "../css/header.module.css";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import bwLogo from "../assets/bw_remalogo.png";
 
 export default function Header({ hidden }) {
+  const location = useLocation();
+
   if (hidden) return null;
+
+  const isActive = (path) => location.pathname === path;
 
   return (
     <header className={styles.header}>
@@ -12,16 +16,36 @@ export default function Header({ hidden }) {
         <span className={styles.title}>ReMa</span>
       </Link>
       <nav className={styles.navLinks}>
-        <Link to="/home" className={styles.navLink}>
+        <Link
+          to="/home"
+          className={`${styles.navLink} ${
+            isActive("/home") ? styles.activeNavLink : ""
+          }`}
+        >
           Home
         </Link>
-        <Link to="/add" className={styles.navLink}>
+        <Link
+          to="/add"
+          className={`${styles.navLink} ${
+            isActive("/add") ? styles.activeNavLink : ""
+          }`}
+        >
           New recipe
         </Link>
-        <Link to="/list" className={styles.navLink}>
+        <Link
+          to="/list"
+          className={`${styles.navLink} ${
+            isActive("/list") ? styles.activeNavLink : ""
+          }`}
+        >
           My recipes
         </Link>
-        <Link to="/planshop" className={styles.navLink}>
+        <Link
+          to="/planshop"
+          className={`${styles.navLink} ${
+            isActive("/planshop") ? styles.activeNavLink : ""
+          }`}
+        >
           Plan & Shop
         </Link>
       </nav>
